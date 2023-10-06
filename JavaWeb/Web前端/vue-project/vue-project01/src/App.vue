e0250269e6f04cfb55908685d5ed198aa9bd7ab7
cokelin <1291468128@qq.com>
2023-10-06 11:01:36 +0800

    add JavaWeb project

---
+++ JavaWeb/Web前端/vue-project/vue-project01/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- 需要写好下面的script才能自动补全 -->
+    <element-view></element-view>
+  </div>
+</template>
+
+<script>
+import ElementView from './views/Element/ElementView.vue'
+export default {
+  components: { ElementView },
+  
+}
+</script>
+
+<style>
+</style>
